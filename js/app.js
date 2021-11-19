0bcbc0882b75b300f097cbe00deaa55dd239a00f
IHomov <innapariy@gmail.com>
2021-11-19 20:49:15 +0200

    check type of system
    fixed

--- js/app.js
+++ js/app.js
@@ -30,21 +30,66 @@ function Moon_Algorithm(setValue) {
 }
 
 function ChekType(valueIn) {
-    // перевожу значения с input в массив
-    let inArr = valueIn.split('');
-    let ArrCards = [
-        { name: "masterCard", diapason: [51, 52, 53, 54, 55] },
-        { name: "maestro", diapason: [5018, 5020, 5038, 5893, 6304, 6759, 6761, 6762, 6763] },
-        { name: "visa", diapason: [4] },
+    let namecard; // переменная для хранения имени типа банковской системы
+
+    let byTwo = +valueIn.match(/.{1,2}/g)[0]; //разбиваю массив для дальнейшего сравнения на масив по 2 цифры
+    let byFour = +valueIn.match(/.{1,4}/g)[0]; // по 4 цифры
+    let byOne = +valueIn.match(/.{1,1}/g)[0]; //по 1 цифры
+
+    let newArr = []; // массив из введенных чисел, который состоит из разбитого входящего числа по 2 цифры, по 4, по 1.
+    newArr.push(byTwo, byFour, byOne);
+    newArr.sort(function (a, b) {
+        return b - a;
+    });
+
+
+    let paymentSystems = [
+        { bin: 51, title: 'masterCard' },
+        { bin: 5018, title: 'maestro' },
+        { bin: 5020, title: 'maestro' },
+        { bin: 5038, title: 'maestro' },
+        { bin: 5893, title: 'maestro' },
+        { bin: 6304, title: 'maestro' },
+        { bin: 6759, title: 'maestro' },
+        { bin: 6761, title: 'maestro' },
+        { bin: 6762, title: 'maestro' },
+        { bin: 6763, title: 'maestro' },
+        { bin: 4, title: 'visa' },
     ]
-    // как добавить интервал чисел в массив diapason???
-    // for (let i=2221; i<=2720; i++) {
-    //         ArrCards[masterCard].value.push(i);
-    //     }
 
-    let typecart = ArrCards.find(el => el.diapason == inArr[i]);
-    let rez = typecart.name;
-    Status.innerHTML = rez;
+    for (let i = 52; i <= 55; i++) {
+        paymentSystems.push({ bin: i, title: "masterCard" });
+    }
     
+    for (let i = 2221; i <= 2720; i++) {
+        paymentSystems.push({ bin: i, title: "masterCard" });
+    }
+    
+    paymentSystems.sort(function (a, b) {
+        return b.bin - a.bin;
+    });
 
+
+function FindEl(ArrayOne, id) {
+        let findedEl = ArrayOne.find(el => el.bin == id);
+        if (findedEl) {
+            namecard = findedEl.title;
+        }
+        else {
+            namecard = "another card";
+        }        
+    }
+
+
+
+    for (let i = 0; i <= newArr.length; i++) {
+        FindEl(paymentSystems, newArr[i]);
+        if (namecard == "another card") {
+            FindEl(paymentSystems, newArr[i + 1]);
+        }
+        else break;
+        
+    }
+    return Typecard.innerHTML = namecard;
 }
+
